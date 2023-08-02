0a90b278396121014c9006cd1507b61dc9f06312
hyejin <obndo8539@naver.com>
2023-08-02 14:11:31 +0900

    quotes 랜덤으로 가져오기 성공 / timer,cpm 미완성

--- src/main/resources/static/js/mainpage.js
+++ src/main/resources/static/js/mainpage.js
@@ -1,8 +1,18 @@
 window.onload = () => {
 
-    MainPageService.getInstance().loadQuotes();
+    MainPageService.getInstance().getRandomQuote();
+    MainPageService.getInstance().addEventInput();
+    MainPageService.getInstance().addStartButton();
 
 }
+
+const quoteDisplayElement = document.getElementById('quoteDisplay');
+const quoteInputElement = document.getElementById('quoteInput');
+const timerElement = document.getElementById('timer');
+const startElement = document.getElementById('start');
+const reloadElement= document.getElementById('reload');
+
+const isTimerRunning = false;
 
 class MainPageApi {
     static #instance = null;
@@ -23,7 +33,7 @@ class MainPageApi {
             dataType: "json",
             success: response => {
                 responseData = response;
-                console.log(response);
+                // console.log(response);
             },
             error: error => {
                 console.log(error);
@@ -43,18 +53,138 @@ class MainPageService {
         return this.#instance;
     }
 
-    loadQuotes() {
-        const quoteDisplayElement = document.getElementById('quoteDisplay');
-        const quoteInputElement = document.getElementById('quoteInput');
+    getRandomQuote() {
         const responseData = MainPageApi.getInstance().searchQuotes();
-        console.log(responseData);
+        const randomIndex = Math.floor(Math.random() * responseData.length);
+        const randomQuote = responseData[randomIndex];
+        const randomQuoteContentKo = randomQuote.contentKo;
 
-        quoteDisplayElement.innerHTML = `
-            <div>${responseData[0].contentKo}<div>
-        `
+        // console.log(randomQuoteContentKo);
+
+        // 랜덤 인용구를 <span> 태그로 나눈다
+        randomQuoteContentKo.split('').forEach(character => {
+            const characterSpan = document.createElement('span')
+            characterSpan.innerText = character
+            quoteDisplayElement.appendChild(characterSpan)
+            // console.log(characterSpan);
+        })
+        quoteInputElement.value = null;
+
+
+        // console.log(responseData);
+
+        // quoteDisplayElement.innerHTML = `
+        //     <div>${randomQuote.contentKo}<div>
+        // `
     }
 
+    addEventInput() {
+        quoteInputElement.addEventListener('input', () => {
+            const arrayQoute = quoteDisplayElement.querySelectorAll('span')
+            const arrayValue = quoteInputElement.value.split('')
 
+            let correct = true
+            let charactersTyped = 0
 
+            arrayQoute.forEach((characterSpan, index) => {
+                const character = arrayValue[index]
+
+                if (character == null) {
+                    characterSpan.classList.remove('correct')
+                    characterSpan.classList.remove('incorrect')
+                    correct = false
+                  } else if (character === characterSpan.innerText) {
+                    characterSpan.classList.add('correct')
+                    characterSpan.classList.remove('incorrect')
+                  } else {
+                    characterSpan.classList.remove('correct')
+                    characterSpan.classList.add('incorrect')
+                    correct = false
+                  }
+            })
+
+            if(correct) {
+                const timeInSeconds = 0;
+                const totalTimeTaken = timeInSeconds;
+                const quoteText = quoteDisplayElement.innerText.trim();
+                const totalCharacters = quoteText.length;
+                const typingSpeed =  this.calculateTypingSpeed(totalCharacters, totalTimeTaken);
+
+                const handleInput = this.handleInput();
+                quoteInputElement.removeEventListener('input', handleInput);
+
+                // Updated: Disable the input field to freeze the screen
+                quoteInputElement.disabled = true;
+
+                // stopTimer();
+
+                Swal.fire({
+                    title: '게임이 끝났습니다! 결과를 저장하시겠습니까?',
+                    html: `타자 속도: ${typingSpeed} CPM \n`,
+                    showDenyButton: true,
+                    confirmButtonText: 'Save',
+                    denyButtonText: `Don't save`,
+                  }).then((result) => {
+                    /* Read more about isConfirmed, isDenied below */
+                    if (result.isConfirmed) {
+                      Swal.fire('Saved!', '', 'success')
+                    } else if (result.isDenied) {
+                      Swal.fire('Changes are not saved', '', 'info')
+                    }
+                  })
+            }
+
+        })
+    }
+
+    calculateTypingSpeed(totalCharacters, timeTaken) {
+        const charactersPerMinute = (totalCharacters / timeTaken) * 60;
+        return charactersPerMinute .toFixed(2);
+    }
+
+    handleInput() {
+        this.charactersTyped++;
+        const totalTimeTaken = this.getTimerTime();
+        const cpm = this.calculateTypingSpeed(this.charactersTyped, totalTimeTaken);
+        // displayCPM(cpm)
+    }
+
+    getTimerTime() {
+        let timeInSeconds = 0;
+
+        timeInSeconds = Math.floor((new Date() - this.startTime) / 1000);
+
+        if (timeInSeconds < 60) {
+            // If less than 60 seconds, display only seconds
+            return timeInSeconds + ' 초';
+        } else {
+            // If 60 seconds or more, convert to minutes and seconds
+            const minutes = Math.floor(timeInSeconds / 60);
+            const seconds = timeInSeconds % 60;
+
+            if (seconds === 0) {
+            return minutes + ' 분';
+            } else {
+            return minutes + ' 분 ' + seconds + ' 초';
+            }
+        }
+    }
+
+    startTimer() {
+        timerElement.innerText = 0;
+        startTime = new Date();
+        isTimerRunning = true;
+        timerInterval = setInterval(() => {
+            timerElement.innerText = this.getTimerTime();
+        }, 1000);
+    }
+
+    // isTimerRunning = false;
+    addStartButton() {
+
+        startElement.onclick = () => {
+            quoteInputElement.disabled = false;
+        }
+    }
 
 }
